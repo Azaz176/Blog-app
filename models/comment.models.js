3b746712516d20e8619ca00559db7dcac98cc084
Azaz <azaz.ali1706@gmail.com>
2024-07-18 01:02:05 +0530

    comments route

--- models/comment.models.js
+++ models/comment.models.js
@@ -1,8 +1,18 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 const commentSchema= new Schema(
     {
         content:{
             type:String,
             required:true
+        },
+        blogId:{
+            type:Schema.Types.ObjectId,
+            ref:"Blog"
+        },
+        createdBy:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
         }
 }, {timestamps:true})
+
+export const Comment= mongoose.model('Comment', commentSchema)
